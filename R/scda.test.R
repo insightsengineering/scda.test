0614578b592b49213fe25a970de2b44b3a6cf395
Davide Garolini <dgarolini@gmail.com>
2023-02-09 23:44:38 +0100

    Adding tests and fixing staged deps (#4)

---
+++ R/scda.test.R
@@ -0,0 +1,14 @@
+#' scda.test: Testing `tern` and `scda` Data
+#'
+#' @description This package provides a testing suite for templates based on
+#'   `scda` data and `tern` analysis methods.
+#'
+#' @details To learn mode about the package, check the documentation of [tern].
+#'
+#' @keywords internal
+"_PACKAGE"
+
+
+# Fix R CMD check notes
+#' @import tern scda scda.2022
+NULL
