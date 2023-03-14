6002e555cd1dfb105ec74b7b94be8bd2e762cc7e
Melkiades <davide.garolini@roche.com>
2023-03-14 16:48:31 +0100

    better to have it separate from the definition of table itself

---
+++ tests/testthat/test-table_pagination.R
@@ -0,0 +1,181 @@
+# Data pre-processing
+adsl <- adsl_raw
+adae <- adae_raw %>%
+  dplyr::mutate(
+    AEDECOD = as.character(AEDECOD),
+    AEBODSYS = as.character(AEBODSYS),
+  )
+gr_grp <- list(
+  "- Any Grade -" = c("1", "2", "3", "4", "5"),
+  "Grade 1-2" = c("1", "2"),
+  "Grade 3-4" = c("3", "4"),
+  "Grade 5" = "5"
+)
+
+# Raw result for future pruning
+raw_result <- basic_table() %>%
+  split_cols_by("ACTARM") %>%
+  add_colcounts() %>%
+  count_occurrences_by_grade(
+    var = "AETOXGR",
+    grade_groups = gr_grp
+  ) %>%
+  split_rows_by("AEBODSYS",
+                split_fun = trim_levels_in_group("AETOXGR"),
+                child_labels = "visible", nested = TRUE
+  ) %>%
+  summarize_occurrences_by_grade(
+    var = "AETOXGR",
+    grade_groups = gr_grp
+  ) %>%
+  split_rows_by("AEDECOD",
+                split_fun = trim_levels_in_group("AETOXGR"),
+                child_labels = "visible", nested = TRUE
+  ) %>%
+  summarize_num_patients(
+    var = "USUBJID",
+    .stats = "unique",
+    .labels = "- Any Grade -"
+  ) %>%
+  count_occurrences_by_grade(
+    var = "AETOXGR",
+    grade_groups = gr_grp[-1],
+    .indent_mods = -1L
+  ) %>%
+  build_table(adae, alt_counts_df = adsl) %>%
+  sort_at_path(
+    path = "AEBODSYS",
+    scorefun = cont_n_allcols,
+    decreasing = TRUE
+  ) %>%
+  sort_at_path(
+    path = c("AEBODSYS", "*", "AEDECOD"),
+    scorefun = cont_n_allcols,
+    decreasing = TRUE
+  )
+
+testthat::test_that("AET04 variant 1 extensive pagination tests", {
+  # Standard, direct call
+  pag_res <- testthat::expect_silent(paginate_table(raw_result))
+  testthat::expect_equal(sapply(pag_res, nrow), c(55, 22))
+  testthat::expect_equal(sapply(pag_res, ncol), c(3, 3))
+
+  # With combo of rows and cols per page
+  std_col_widths <- formatters::propose_column_widths(raw_result)
+  # Min h-width: first value is the row label, second is the largest content, 3 is the inter-space
+  cpp_width <- std_col_widths[1] + max(std_col_widths[-1]) + 3
+  pag_res <- testthat::expect_silent(
+    paginate_table(raw_result, cpp = cpp_width, lpp = 12) # 12 is the minimum -> automatic way?
+  )
+  testthat::expect_equal(length(pag_res), 11 * 3) # 11 because 3 branches (out of 8) have 2 leaves
+  testthat::expect_snapshot(pag_res[9:10])
+
+
+  # More complicated table options (i.e. having wrapping on)
+  res <- testthat::expect_silent(raw_result)
+
+  main_title(res) <- "main title with some new \nline"
+  subtitles(res) <- c("sub", "-------", "titles")
+  main_footer(res) <- "main footer"
+  prov_footer(res) <- "prov \nfooter that has a lot of \nnew \nlines"
+  top_left(res) <- "SOME TOPLEFT"
+  table_inset(res) <- 5
+  clw <- c(10, 8, 8, 10)
+  lpp_tmp <- 36
+
+  pg_tbl_w_clw <- paginate_table(res, lpp = lpp_tmp, colwidths = clw)
+  pg_tbl_no_clw <- paginate_table(res, lpp = lpp_tmp)
+  res1 <- toString(pg_tbl_no_clw[[1]], widths = clw)
+  res2 <- toString(res[
+    1:17,
+    keep_titles = TRUE,
+    keep_footers = TRUE,
+    keep_topleft = TRUE
+  ], widths = clw)
+
+  testthat::expect_identical(res1, res2)
+
+  testthat::expect_snapshot(cat(res1))
+  testthat::expect_snapshot(cat(toString(pg_tbl_no_clw[[3]], widths = clw)))
+
+  # Other parameters: page type
+  pag_res <- paginate_table(res, page_type = "letter")
+  testthat::expect_equal(sapply(pag_res, nrow), c(43, 39))
+  testthat::expect_snapshot(pag_res[2])
+  pag_res <- paginate_table(res, page_type = "a4")
+  testthat::expect_equal(sapply(pag_res, nrow), c(47, 47, 30, 30))
+  testthat::expect_snapshot(pag_res[4])
+  pag_res <- paginate_table(res, page_type = "legal")
+  testthat::expect_equal(sapply(pag_res, nrow), c(61, 16))
+  testthat::expect_snapshot(pag_res[3])
+
+  # Other parameters: page height and width
+  pag_res <- paginate_table(res, pg_width = 7, pg_height = 10)
+  testthat::expect_equal(sapply(pag_res, nrow), c(35, 35, 34, 34, 8, 8))
+  testthat::expect_snapshot(pag_res[2])
+
+  # Other parameters: all together
+  pag_res <- paginate_table(
+    res,
+    landscape = TRUE,
+    lineheight = 2,
+    font_size = 9,
+    font_family = "Courier",
+    margins = c(top = 1, bottom = 1, left = 3, right = 3)
+  )
+  testthat::expect_equal(
+    sapply(pag_res, nrow),
+    c(8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 8, 8, 9, 9, 8, 8, 6, 6, 8, 8, 8, 8)
+  )
+  testthat::expect_snapshot(pag_res[5:10])
+})
+
+testthat::test_that("AET04 variant 2 page_by pagination tests", {
+  adae <- adae %>%
+    dplyr::filter(ACTARM == "A: Drug X")
+
+  lyt <- basic_table() %>%
+    split_cols_by("ACTARM") %>%
+    add_colcounts() %>%
+    split_rows_by("AEBODSYS",
+                  split_fun = trim_levels_in_group("AETOXGR"),
+                  child_labels = "visible", nested = TRUE,
+                  page_by = TRUE
+    ) %>%
+    summarize_occurrences_by_grade(
+      var = "AETOXGR",
+      grade_groups = gr_grp
+    ) %>%
+    split_rows_by("AEDECOD",
+                  split_fun = trim_levels_in_group("AETOXGR"),
+                  child_labels = "visible", nested = TRUE
+    ) %>%
+    summarize_num_patients(
+      var = "USUBJID",
+      .stats = "unique",
+      .labels = "- Any Grade -"
+    ) %>%
+    count_occurrences_by_grade(
+      var = "AETOXGR",
+      grade_groups = gr_grp[-1],
+      .indent_mods = -1L
+    )
+
+  result <- lyt %>%
+    build_table(adae, alt_counts_df = adsl) %>%
+    sort_at_path(
+      path = "AEBODSYS",
+      scorefun = cont_n_allcols,
+      decreasing = TRUE
+    ) %>%
+    sort_at_path(
+      path = c("AEBODSYS", "*", "AEDECOD"),
+      scorefun = cont_n_allcols,
+      decreasing = TRUE
+    )
+
+  res <- testthat::expect_silent(result)
+  pag_res <- testthat::expect_silent(paginate_table(res))
+  testthat::expect_identical(names(pag_res), levels(adae$AEBODSYS))
+  testthat::expect_snapshot(pag_res[c(2, 5)])
+})
