3bf8b36471e382c0572b5a5f471f91d15cedd1fb
Melkiades <davide.garolini@roche.com>
2023-03-17 14:39:50 +0100

    fixes following discussion

--- tests/testthat/test-table_pagination.R
+++ tests/testthat/test-table_pagination.R
@@ -1,3 +1,6 @@
+# All of these tests are based on AET04 variant 1 and mainly concern pagination,
+# i.e. paginate_table and its parameters.
+
 # Data pre-processing
 adsl <- adsl_raw
 adae <- adae_raw %>%
@@ -54,12 +57,14 @@ raw_result <- basic_table() %>%
     decreasing = TRUE
   )
 
-testthat::test_that("AET04 variant 1 extensive pagination tests", {
+testthat::test_that("Direct pagination with standard values", {
   # Standard, direct call
   pag_res <- testthat::expect_silent(paginate_table(raw_result))
   testthat::expect_equal(sapply(pag_res, nrow), c(55, 22))
   testthat::expect_equal(sapply(pag_res, ncol), c(3, 3))
+})
 
+testthat::test_that("Pagination with specific column widths and minimum lines per page", {
   # With combo of rows and cols per page
   std_col_widths <- formatters::propose_column_widths(raw_result)
   # Min h-width: first value is the row label, second is the largest content, 3 is the inter-space
@@ -68,20 +73,25 @@ testthat::test_that("AET04 variant 1 extensive pagination tests", {
     paginate_table(raw_result, cpp = cpp_width, lpp = 12) # 12 is the minimum -> automatic way?
   )
   testthat::expect_equal(length(pag_res), 11 * 3) # 11 because 3 branches (out of 8) have 2 leaves
-  testthat::expect_snapshot(pag_res[9:10])
+  testthat::expect_snapshot(pag_res[9:10]) # randomly picked to have a comparison
+})
 
+# More decoration
+res <- raw_result
+main_title(res) <- "main title with some new \nline"
+subtitles(res) <- c("sub", "-------", "titles")
+main_footer(res) <- "main footer"
+prov_footer(res) <- "prov \nfooter that has a lot of \nnew \nlines"
+top_left(res) <- "SOME TOPLEFT"
+table_inset(res) <- 5
+rtables::fnotes_at_path(res,
+                        rowpath = c(make_row_df(res)$path[[7]]),
+                        colpath = c("B: Placebo")) <- "Some notes"
 
-  # More complicated table options (i.e. having wrapping on)
-  res <- testthat::expect_silent(raw_result)
+testthat::test_that("Pagination works also if table is decorated", {
 
-  main_title(res) <- "main title with some new \nline"
-  subtitles(res) <- c("sub", "-------", "titles")
-  main_footer(res) <- "main footer"
-  prov_footer(res) <- "prov \nfooter that has a lot of \nnew \nlines"
-  top_left(res) <- "SOME TOPLEFT"
-  table_inset(res) <- 5
-  clw <- c(10, 8, 8, 10)
-  lpp_tmp <- 36
+  clw <- c(10, 8, 8, 10) # random values to have "some" wrapping
+  lpp_tmp <- 36 # minimum for this table
 
   pg_tbl_w_clw <- paginate_table(res, lpp = lpp_tmp, colwidths = clw)
   pg_tbl_no_clw <- paginate_table(res, lpp = lpp_tmp)
@@ -94,26 +104,35 @@ testthat::test_that("AET04 variant 1 extensive pagination tests", {
   ], widths = clw)
 
   testthat::expect_identical(res1, res2)
+  testthat::expect_equal(length(pg_tbl_no_clw), length(pg_tbl_w_clw) - 5)
 
   testthat::expect_snapshot(cat(res1))
-  testthat::expect_snapshot(cat(toString(pg_tbl_no_clw[[3]], widths = clw)))
 
+  testthat::expect_snapshot(cat(toString(pg_tbl_no_clw[[3]], widths = clw)))
+  testthat::expect_snapshot(cat(toString(pg_tbl_w_clw[[3]], widths = clw)))
+})
+
+testthat::test_that("Pagination works for page types", {
   # Other parameters: page type
   pag_res <- paginate_table(res, page_type = "letter")
-  testthat::expect_equal(sapply(pag_res, nrow), c(43, 39))
+  testthat::expect_snapshot(sapply(pag_res, nrow))
   testthat::expect_snapshot(pag_res[2])
   pag_res <- paginate_table(res, page_type = "a4")
-  testthat::expect_equal(sapply(pag_res, nrow), c(47, 47, 30, 30))
+  testthat::expect_snapshot(sapply(pag_res, nrow))
   testthat::expect_snapshot(pag_res[4])
   pag_res <- paginate_table(res, page_type = "legal")
-  testthat::expect_equal(sapply(pag_res, nrow), c(61, 16))
+  testthat::expect_snapshot(sapply(pag_res, nrow))
   testthat::expect_snapshot(pag_res[3])
+})
 
+testthat::test_that("Pagination works for page width and height", {
   # Other parameters: page height and width
   pag_res <- paginate_table(res, pg_width = 7, pg_height = 10)
-  testthat::expect_equal(sapply(pag_res, nrow), c(35, 35, 34, 34, 8, 8))
+  testthat::expect_snapshot(sapply(pag_res, nrow))
   testthat::expect_snapshot(pag_res[2])
+})
 
+testthat::test_that("Pagination works for page types", {
   # Other parameters: all together
   pag_res <- paginate_table(
     res,
@@ -123,10 +142,7 @@ testthat::test_that("AET04 variant 1 extensive pagination tests", {
     font_family = "Courier",
     margins = c(top = 1, bottom = 1, left = 3, right = 3)
   )
-  testthat::expect_equal(
-    sapply(pag_res, nrow),
-    c(8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 8, 8, 9, 9, 8, 8, 6, 6, 8, 8, 8, 8)
-  )
+  testthat::expect_snapshot(sapply(pag_res, nrow))
   testthat::expect_snapshot(pag_res[5:10])
 })
 
