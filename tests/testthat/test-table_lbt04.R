0baf58e8052f29811d13bffdfdc525cb6a086ac6
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-06-27 17:25:24 -0400

    Update LBT template tests that differ in chevron

--- tests/testthat/test-table_lbt04.R
+++ tests/testthat/test-table_lbt04.R
@@ -3,23 +3,32 @@
 adsl <- adsl_raw
 adlb <- adlb_raw
 
-testthat::test_that("LBT04 default variant is produced correctly", {
-  # Note: We exclude "SCREENING" visit here since otherwise it would be used as post-baseline below.
-  adlb <- adlb %>%
-    dplyr::filter(AVISIT != "SCREENING") %>%
-    dplyr::mutate(AVISIT = droplevels(AVISIT))
+adsl <- df_explicit_na(adsl)
+adlb <- df_explicit_na(adlb)
 
-  result <- basic_table() %>%
-    split_cols_by("ARM") %>%
-    add_colcounts() %>%
-    split_rows_by("LBCAT") %>%
-    split_rows_by("PARAM", split_fun = drop_split_levels) %>%
+adlb_f <- adlb %>%
+  filter(ONTRTFL == "Y", ANRIND != "<Missing>") %>%
+  var_relabel(
+    PARAM = "Laboratory Test",
+    ANRIND = "Direction of Abnormality"
+  )
+
+testthat::test_that("LBT04 default variant is produced correctly", {
+  lyt <- basic_table(show_colcounts = TRUE) %>%
+    split_cols_by("ACTARM") %>%
+    split_rows_by("PARAM",
+                  split_fun = drop_split_levels,
+                  label_pos = "topleft",
+                  split_label = obj_label(adlb_f$PARAM)
+    ) %>%
     count_abnormal(
       var = "ANRIND",
-      abnormal = list(Low = "LOW", High = "HIGH"),
+      abnormal = list(Low = c("LOW", "LOW LOW"), High = c("HIGH", "HIGH HIGH")),
       exclude_base_abn = TRUE
     ) %>%
-    build_table(adlb, alt_counts_df = adsl)
+    append_varlabels(adlb_f, "ANRIND", indent = 1L)
+
+  result <- build_table(lyt = lyt, df = adlb_f, alt_counts_df = adsl)
 
   res <- testthat::expect_silent(result)
   testthat::expect_snapshot(res)
