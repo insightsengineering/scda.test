c4990adcfb280b83bcd75229f43c95b10711270f
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-07-06 14:42:05 -0400

    Update safety template tests that differ in chevron

--- tests/testthat/test-table_vst02.R
+++ tests/testthat/test-table_vst02.R
@@ -3,31 +3,49 @@
 adsl <- adsl_raw
 advs <- advs_raw
 
-testthat::test_that("1. Vital Sign Abnormalities (Regardless of Abnormality at Baseline, VST02_1)", {
-  # Note: We keep only post-baseline for analysis.
-  advs_f <- advs %>% dplyr::filter(ABLFL != "Y" & ABLFL2 != "Y")
+adsl <- df_explicit_na(adsl)
+advs <- df_explicit_na(advs)
 
-  result <- basic_table() %>%
-    split_cols_by("ARM", split_fun = add_overall_level("All Patients", first = FALSE)) %>%
-    add_colcounts() %>%
-    split_rows_by("PARAM", split_label = c("Parameter / Abnormality Direction"), label_pos = "visible") %>%
-    count_abnormal("ANRIND", abnormal = list(Low = "LOW", High = "HIGH")) %>%
-    build_table(df = advs_f, alt_counts_df = adsl)
+advs_f <- advs %>%
+  filter(ONTRTFL == "Y", ANRIND != "<Missing>") %>%
+  var_relabel(
+    PARAM = "Assessment",
+    ANRIND = "Abnormality"
+  )
+
+split_fun <- drop_split_levels
+
+testthat::test_that("1. Vital Sign Abnormalities (Regardless of Abnormality at Baseline, VST02_1)", {
+  lyt <- basic_table(show_colcounts = TRUE) %>%
+    split_cols_by(var = "ACTARM") %>%
+    add_overall_col("All Patients") %>%
+    split_rows_by("PARAM", split_fun = split_fun, label_pos = "topleft", split_label = obj_label(advs_f$PARAM)) %>%
+    count_abnormal(
+      "ANRIND",
+      abnormal = list(Low = c("LOW", "LOW LOW"), High = c("HIGH", "HIGH HIGH")),
+      exclude_base_abn = FALSE
+    ) %>%
+    append_varlabels(advs_f, "ANRIND", indent = 1L)
+
+  result <- build_table(lyt = lyt, df = advs_f, alt_counts_df = adsl)
 
   res <- testthat::expect_silent(result)
   testthat::expect_snapshot(res)
 })
 
 testthat::test_that("2. Vital Sign Abnormalities (Among Subject Without Abnormality at Baseline, VST02_2)", {
-  # Note: We keep only post-baseline for analysis.
-  advs_f <- advs %>% dplyr::filter(AVISITN > 0)
+  lyt <- basic_table(show_colcounts = TRUE) %>%
+    split_cols_by(var = "ACTARM") %>%
+    add_overall_col("All Patients") %>%
+    split_rows_by("PARAM", split_fun = split_fun, label_pos = "topleft", split_label = obj_label(advs_f$PARAM)) %>%
+    count_abnormal(
+      "ANRIND",
+      abnormal = list(Low = c("LOW", "LOW LOW"), High = c("HIGH", "HIGH HIGH")),
+      exclude_base_abn = TRUE
+    ) %>%
+    append_varlabels(advs_f, "ANRIND", indent = 1L)
 
-  result <- basic_table() %>%
-    split_cols_by("ARM", split_fun = add_overall_level("All Patients", first = FALSE)) %>%
-    add_colcounts() %>%
-    split_rows_by("PARAM", split_label = c("Parameter / Abnormality Direction"), label_pos = "visible") %>%
-    count_abnormal("ANRIND", abnormal = list(Low = "LOW", High = "HIGH"), exclude_base_abn = TRUE) %>%
-    build_table(df = advs_f, alt_counts_df = adsl)
+  result <- build_table(lyt = lyt, df = advs_f, alt_counts_df = adsl)
 
   res <- testthat::expect_silent(result)
   testthat::expect_snapshot(res)
