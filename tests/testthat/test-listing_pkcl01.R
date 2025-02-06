4840cec4245257c7ad4d9106bac5c6111a653894
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2025-02-06 17:36:26 -0500

    Add tests for tern `tabulate_rsp_subgroups` bug and formatters key columns bug (#182)
    Closes #181

--- tests/testthat/test-listing_pkcl01.R
+++ tests/testthat/test-listing_pkcl01.R
@@ -27,3 +27,31 @@ testthat::test_that("PKCL01 listing is produced correctly", {
 
   testthat::expect_snapshot(result)
 })
+
+testthat::test_that("PKCL01 listing key columns values are repeated when entries span multiple pages", {
+  adpc <- adpc_pharmaverse
+  out <- adpc %>%
+    select(TRT01A, USUBJID, VISIT, NFRLT, AFRLT, AVAL)
+
+  var_labels(out) <- c(
+    TRT01A = "Treatment Group",
+    USUBJID = "Subject ID",
+    VISIT = "Visit",
+    NFRLT = paste0("Nominal Sampling\nTime (", adpc$FRLTU[1], ")"),
+    AFRLT = paste0("Actual Time\nFrom First\nDose (", adpc$FRLTU[1], ")"),
+    AVAL = paste0("Concentration\n(", adpc$AVALU[1], ")")
+  )
+
+  out <- out %>%
+    arrange(TRT01A, USUBJID, NFRLT)
+
+  result <- as_listing(
+    out,
+    key_cols = c("TRT01A", "USUBJID", "VISIT"),
+    disp_cols = names(out)
+  ) %>%
+    head(50) %>%
+    export_as_txt(tf_wrap = TRUE, cpp = 120, lpp = 30)
+
+  testthat::expect_snapshot(result %>% cat())
+})
