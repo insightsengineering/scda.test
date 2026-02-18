4711d9b497d8164b58a77d9e5ce0bf364d3fc58f
Joe Zhu <sha.joe.zhu@gmail.com>
2026-02-19 00:12:56 +0800

    resthye

--- tests/testthat/test-char-support.R
+++ tests/testthat/test-char-support.R
@@ -4,45 +4,45 @@ adeg <- random.cdisc.data::cadeg
 ################################################################################
 # Define script level parameters:
 ################################################################################
-adsl_f <- adsl %>%
+adsl_f <- adsl |>
   select(USUBJID, ACTARM)
 
 # Note:  We keep only post-baseline for analysis.
-adeg_f <- adeg %>%
-  filter(AVISIT %in% levels(adeg$AVISIT)[1:3]) %>%
-  filter(PARAM %in% c("Heart Rate", "QT Duration", "RR Duration")) %>%
+adeg_f <- adeg |>
+  filter(AVISIT %in% levels(adeg$AVISIT)[1:3]) |>
+  filter(PARAM %in% c("Heart Rate", "QT Duration", "RR Duration")) |>
   var_relabel(
     PARAM = "Assessment",
     ANRIND = "Abnormality"
-  ) %>%
-  select(USUBJID, AVISIT, PARAM, PARAMCD, ANRIND, ACTARM) %>%
+  ) |>
+  select(USUBJID, AVISIT, PARAM, PARAMCD, ANRIND, ACTARM) |>
   mutate(PARCAT = case_when(
     PARAMCD == "QT" ~ "QT",
     TRUE ~ "Other"
-  )) %>%
-  mutate(ANRIND2 = paste(ANRIND, PARAMCD)) %>%
+  )) |>
+  mutate(ANRIND2 = paste(ANRIND, PARAMCD)) |>
   mutate(ANRIND3 = paste(ANRIND, PARAM))
 
 # Ensure character variables are converted to factors and empty strings and NAs are explicit missing levels.
 adsl_f <- df_explicit_na(adsl_f)
 adeg_f <- df_explicit_na(adeg_f)
 
-adeg_f_1 <- adeg_f %>%
-  filter(PARAMCD != "QT") %>%
-  mutate(CRITDIR = "Direction 1") %>%
+adeg_f_1 <- adeg_f |>
+  filter(PARAMCD != "QT") |>
+  mutate(CRITDIR = "Direction 1") |>
   mutate(ANRIND23 = ANRIND2)
 
-adeg_f_2 <- adeg_f %>%
-  mutate(CRITDIR = "Direction 2") %>%
+adeg_f_2 <- adeg_f |>
+  mutate(CRITDIR = "Direction 2") |>
   mutate(ANRIND23 = ANRIND3)
 
 adeg_f <- rbind(adeg_f_1, adeg_f_2)
 
 
 ### mapping dataframe
-uniq_adeg <- unique(adeg_f %>% select(PARCAT, PARAMCD, CRITDIR, ANRIND23))
-mapdf <- uniq_adeg %>%
-  arrange(PARCAT, PARAMCD, CRITDIR, ANRIND23) %>%
+uniq_adeg <- unique(adeg_f |> select(PARCAT, PARAMCD, CRITDIR, ANRIND23))
+mapdf <- uniq_adeg |>
+  arrange(PARCAT, PARAMCD, CRITDIR, ANRIND23) |>
   mutate(across(where(is.factor), as.character))
 
 ### 3 versions of dataset
@@ -51,7 +51,7 @@ adeg_f1 <- adeg_f
 
 ### CRITDIR not factor, not all visits available data for all PARAMCD
 ### exclude QT from screening - this will generate the problem in 6.11
-adeg_f <- adeg_f %>%
+adeg_f <- adeg_f |>
   filter(!(AVISIT == "SCREENING" & PARAMCD == "QT"))
 
 ### CRITDIR factor, not all visits available data
@@ -68,29 +68,29 @@ adeg_f2$CRITDIR <- factor(adeg_f2$CRITDIR)
 lyt <- basic_table(
   show_colcounts = TRUE,
   colcount_format = "N=xx"
-) %>%
-  split_cols_by("ACTARM") %>%
+) |>
+  split_cols_by("ACTARM") |>
   split_rows_by("AVISIT",
     label_pos = "topleft",
     section_div = " ",
     split_fun = drop_split_levels,
     split_label = "Time Point",
     child_labels = "visible"
-  ) %>%
+  ) |>
   split_rows_by("PARAMCD",
     split_label = "Test",
     label_pos = "topleft",
     indent_mod = 1L,
     child_labels = "visible",
     split_fun = trim_levels_to_map(mapdf)
-  ) %>%
+  ) |>
   split_rows_by("CRITDIR",
     split_label = "Direction",
     label_pos = "topleft",
     indent_mod = 1L,
     child_labels = "visible",
     split_fun = trim_levels_to_map(mapdf)
-  ) %>%
+  ) |>
   analyze(c("ANRIND23"),
     a_summary,
     extra_args = list(.stats = "count_fraction"),
